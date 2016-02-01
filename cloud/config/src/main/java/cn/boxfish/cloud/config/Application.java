56c1f62bfa059139efc3d120b114ad882df8d782
undancer <undancer@gmail.com>
2016-02-01 10:28:19 +0800

    添加config的配置

--- cloud/config/src/main/java/cn/boxfish/cloud/config/Application.java
+++ cloud/config/src/main/java/cn/boxfish/cloud/config/Application.java
@@ -2,14 +2,14 @@ package cn.boxfish.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by undancer on 16/1/30.
  */
 @SpringBootApplication
-@SpringCloudApplication
+@EnableDiscoveryClient
 @EnableConfigServer
 public class Application {
     public static void main(String[] args) {
